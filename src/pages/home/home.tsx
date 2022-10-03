e88058a73a268876d39e8f19deaf5f8d2682316c
yen <l.yennie.j@gmail.com>
2022-10-03 14:44:16 +0900

    typescript project

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+
+function Home() {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Home;
